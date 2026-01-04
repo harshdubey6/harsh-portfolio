e8f6ddb0c122dfae80d3a5aaf218e8409e5ac90a
harsh dubey <harshtroublemaker@gmail.com>
2026-01-04 15:41:42 +0530

    layout

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Harsh Dubey - Full Stack Developer",
+  title: "Harsh Dubey ",
   description: "Portfolio website of Harsh Dubey, a Full Stack Developer specializing in modern web technologies.",
 }; 
